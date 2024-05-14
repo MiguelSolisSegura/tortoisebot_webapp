b40d827c9b132e6bd72396d9fe230f651c6fc05a
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-05-14 21:32:14 +0000

    Get camera image

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ var app = new Vue({
                 this.connected = true
                 this.loading = false
                 this.setup3DViewer()
+                this.setCamera()
             })
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
@@ -39,6 +40,7 @@ var app = new Vue({
                 this.loading = false
                 document.getElementById('map').innerHTML = ''
                 this.unset3DViewer()
+                document.getElementById('divCamera').innerHTML = ''
             })
              this.mapViewer = new ROS2D.Viewer({
                 divID: 'map',
@@ -101,6 +103,22 @@ var app = new Vue({
         },
         unset3DViewer() {
             document.getElementById('div3DViewer').innerHTML = ''
+        },
+        // Camera functionality
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera',
+                host: host,
+                width: 640,
+                height: 480,
+                topic: '/camera/image_raw',
+                ssl: true,
+            })
         },
     },
     mounted() {
