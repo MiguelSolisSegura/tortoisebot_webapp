3dfc1f5e9338100edfd0cefee3c0f3445d3774e2
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-05-15 00:22:46 +0000

    Include joystick

--- main.js
+++ main.js
@@ -16,6 +16,25 @@ var app = new Vue({
         viewer: null,
         tfClient: null,
         urdfClient: null,
+        // Dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+        // Joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // Publisher
+        pubInterval: null,
     },
     // helper methods to connect to ROS
     methods: {
@@ -30,6 +49,7 @@ var app = new Vue({
                 this.loading = false
                 this.setup3DViewer()
                 this.setCamera()
+                this.pubInterval = setInterval(this.publish, 100)
             })
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
@@ -62,6 +82,18 @@ var app = new Vue({
         },
         disconnect: function() {
             this.ros.close()
+        },
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -this.joystick.horizontal, },
+            })
+            topic.publish(message)
         },
         setup3DViewer() {
             this.viewer = new ROS3D.Viewer({
@@ -120,8 +152,60 @@ var app = new Vue({
                 ssl: true,
             })
         },
+        // Joystick funtionality
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        },
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
     },
     mounted() {
+        window.addEventListener('mouseup', this.stopDrag)
         this.interval = setInterval(() => {
             if (this.ros != null && this.ros.isConnected) {
                 this.ros.getNodes((data) => { }, (error) => { })
